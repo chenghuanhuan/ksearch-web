6b6d1d4b8dac184fac48892bcc5ab04157912a32
chenghuanhuan <ch746848281@163.com>
2018-02-06 14:55:39 +0800

    乱码

--- ksearch-web-biz/src/main/java/la/kaike/ksearch/biz/service/impl/BaseService.java
+++ ksearch-web-biz/src/main/java/la/kaike/ksearch/biz/service/impl/BaseService.java
@@ -20,6 +20,7 @@ import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.util.*;
 
@@ -72,7 +73,7 @@ public class BaseService {
         return pageResponse;
     }
 
-    protected BoolQueryBuilder query(BaseRequest request,Class<?> clazz){
+    protected BoolQueryBuilder query(BaseRequest request,Class<?> clazz) throws UnsupportedEncodingException {
         BoolQueryBuilder boolQueryBuilder = boolQuery();
 
         Set<Field> fieldSet =  ClassUtils.getAllFiled(clazz);
@@ -98,7 +99,7 @@ public class BaseService {
                         value = ClassUtils.getFieldValue(request,field.getName());
                         if (!StringUtils.isBlank((String)value)) {
                             String val = (String)value;
-                            boolQueryBuilder.must(QueryBuilders.matchQuery(fieldName, val));
+                            boolQueryBuilder.must(QueryBuilders.matchQuery(fieldName, new String(val.getBytes(),"UTF-8")));
                         }
                         break;
 
